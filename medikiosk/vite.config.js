8d89f5c97f3be9dc3a7894de42d5ef2b5a000ef7
ParthShetty0 <vishalas134@gmail.com>
2026-09-07 23:16:20 +0530

    Ngrok Configuration

--- medikiosk/vite.config.js
+++ medikiosk/vite.config.js
@@ -2,14 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
   server: {
-    host: true, // Listens on all local IP addresses (0.0.0.0)
+    host: true,
+    allowedHosts: true,
+    hmr: {
+      clientPort: 443, // Forces Vite HMR to use standard HTTPS port through ngrok
+    },
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
